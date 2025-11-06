9f3394419bd647473c7901c316be76d1ee696b53
ninatoscanelli <nina.toscanelli@studentfr.ch>
2025-11-06 16:26:40 +0100

    Before code revamp

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,21 @@
 import gsap from "gsap";
+import { ScrollTrigger } from "gsap/all";
+gsap.registerPlugin(ScrollTrigger);
+
+gsap.to(".screens-stroll", {
+  x: "-66%",
+  scrollTrigger: {
+    trigger: ".scroller-stroll",
+    pin: true,
+    scrub: 1,
+  },
+});
+
+gsap.to(".screens-hunting", {
+  x: "-66%",
+  scrollTrigger: {
+    trigger: ".scroller-hunting",
+    pin: true,
+    scrub: 1,
+  },
+});
